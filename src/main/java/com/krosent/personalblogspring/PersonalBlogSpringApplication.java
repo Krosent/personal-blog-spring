eaedfe06d25157987522dd542fb5791727beecd6
Artyom Kuznetsov <krosent2@gmail.com>
2021-09-26 01:51:21 +0200

    first commit

---
+++ src/main/java/com/krosent/personalblogspring/PersonalBlogSpringApplication.java
@@ -0,0 +1,13 @@
+package com.krosent.personalblogspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBlogSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalBlogSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krosent/personalblogspring/PersonalBlogSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krosent.personalblogspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBlogSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
