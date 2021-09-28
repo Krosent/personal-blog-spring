dfea06cb4c61542f1fa9450be821cbf02e4764d3
Artyom Kuznetsov <krosent2@gmail.com>
2021-09-28 17:21:36 +0200

    Usage of PagingAndSortingRepository instead of CrudRepository for PostRepository

--- src/main/java/com/krosent/personalblogspring/repositories/PostRepository.java
+++ src/main/java/com/krosent/personalblogspring/repositories/PostRepository.java
@@ -1,17 +1,16 @@
 package com.krosent.personalblogspring.repositories;
-
 import com.krosent.personalblogspring.models.Post;
-import com.krosent.personalblogspring.models.User;
 import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.data.repository.PagingAndSortingRepository;
 import java.util.List;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends PagingAndSortingRepository<Post, Long> {
     @Query("SELECT * FROM post WHERE author_id = :userId")
     public List<Post> findAllPostsOfTheUser(Long userId);
 
     @Query("SELECT * FROM post WHERE featured = true LIMIT 1")
     public Post findFeaturedPost();
+
+
 
 }
